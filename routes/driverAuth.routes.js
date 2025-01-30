5a00af2151feee053d01116f15f463f5ece6cd31
Success-akinyemi <brownsamuel915@gmail.com>
2025-01-30 07:50:29 -0800

    account ID

--- routes/driverAuth.routes.js
+++ routes/driverAuth.routes.js
@@ -3,15 +3,27 @@ import * as controllers from '../controllers/driverAuth.controllers.js'
 import { uploadImages } from '../middlewares/multer.js'
 const router = express.Router()
 
+// Custom error handler for Multer
+const multerErrorHandler = (err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+      if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+        console.error(`Unexpected field: ${err.field}`);
+        return res.status(400).json({ error: `Unexpected field: ${err.field}` });
+      }
+    }
+    console.log('MULTER  ERROR', err)
+    next(err); // Pass to the next error handler if not a Multer error
+  };
+  
 //POST
 router.post('/registerWithPassengerAccount', controllers.registerWithPassengerAccount)
 router.post('/verifyPassengerToDriverAccountOtp', controllers.verifyPassengerToDriverAccountOtp)
-router.post('/completeDriverRegistration', uploadImages, controllers.completeDriverRegistration)
+router.post('/completeDriverRegistration', uploadImages, multerErrorHandler, controllers.completeDriverRegistration)
 router.post('/registerNewDriver', controllers.registerNewDriver)
 router.post('/resendOtp', controllers.resendOtp)
 router.post('/verifyPersonalDetails', controllers.verifyPersonalDetails)
 router.post('/verifySSN', controllers.verifySSN)
-router.post('/completeNewDriverRegistration', uploadImages, controllers.completeNewDriverRegistration)
+router.post('/completeNewDriverRegistration', uploadImages, multerErrorHandler, controllers.completeNewDriverRegistration)
 
 router.post('/signin', controllers.signin)
 router.post('/verifyLoginOtp', controllers.verifyLoginOtp)
